c3eafd0017361842509b0b1c6536d321921d761e
BluesCoder2309 <singh.utkarsh2309@gmail.com>
2025-04-27 14:20:48 +0530

    second commit with some changes

--- quiz.js
+++ quiz.js
@@ -1,53 +1,116 @@
-const questionObj = {
-  category: "Food & Drink",
-  id: "qa-1",
-  correctAnswer: "Three",
-  options: ["Two", "Three", "Four", "Five"],
-  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
-};
+// const questionObj = {
+//   category: "Food & Drink",
+//   id: "qa-1",
+//   correctAnswer: "Three",
+//   options: ["Two", "Three", "Four", "Five"],
+//   question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+// };
 
-// destructured the object because we'll be needing these again for other questions we add
-const { correctAnswer, options, question } = questionObj;
+const quesJSON = [
+  {
+    correctAnswer: "Three ",
+    options: ["Two", "Three ", "Four", "Five"],
+    question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+  },
+  {
+    correctAnswer: "L. Frank Baum",
+    options: [
+      "Suzanne Collins",
+      "James Fenimore Cooper",
+      "L. Frank Baum",
+      "Donna Leon",
+    ],
+    question: "Which author wrote 'The Wonderful Wizard of Oz'?",
+  },
+  {
+    correctAnswer: "Atlanta United",
+    options: [
+      "Atlanta United",
+      "Atlanta Impact",
+      "Atlanta Bulls",
+      "Atlanta Stars",
+    ],
+    question: "Which of these is a soccer team based in Atlanta?",
+  },
+  {
+    correctAnswer: "A Nanny",
+    options: ["A Sow", "A Lioness", "A Hen", "A Nanny"],
+    question: "A female goat is known as what?",
+  },
+  {
+    correctAnswer: "P. L. Travers",
+    options: [
+      "J. R. R. Tolkien",
+      "P. L. Travers",
+      "Lewis Carroll",
+      "Enid Blyton",
+    ],
+    question: "Which author wrote 'Mary Poppins'?",
+  },
+];
+
 let score = 0;
+let currentQuestion = 0;
+const totalScore = quesJSON.length;
 
 //Accesing all the elements
 const questionEl = document.querySelector("#question");
 const optionEl = document.querySelector("#options");
 const scoreEl = document.querySelector("#score");
+const nextEl = document.querySelector('#next');
 
-//Manipulating the DOM
+showQues();
 
-//Setting the question text context
-questionEl.textContent = question;
+nextEl.addEventListener('click', ()=>{
+  scoreEl.textContent = `Score: ${score} / ${totalScore}`;
+  nextQuestion();
+})
 
-const shuffledOptions = shuffleOptions(options);
-//Populating the options div with the buttons
-shuffledOptions.forEach((opt) => {
-  const btn = document.createElement("button");
-  btn.textContent = opt;
-  optionEl.appendChild(btn);
+//Creating a function that displays one after other the questions.
+function showQues() {
+  // destructured the JSON object for each question.
+  const { correctAnswer, options, question } = quesJSON[currentQuestion];
 
-  //Event handling on the button
-  btn.addEventListener("click", function () {
-    if (opt === correctAnswer) {
-      score++;
-    } else {
-      score = score - 0.25;
-    }
-    console.log(score);
-    scoreEl.textContent = `Score: ${score}`;
-    questionEl.textContent = "Quiz Completed!";
-    optionEl.textContent = "";
+  //Setting the question text context
+  questionEl.textContent = question;
+  const shuffledOptions = shuffleOptions(options);
+
+  //Populating the options div with the buttons
+  shuffledOptions.forEach((opt) => {
+    const btn = document.createElement("button");
+    btn.textContent = opt;
+    optionEl.appendChild(btn);
+
+    //Event handling on the button
+    btn.addEventListener("click", function () {
+      if (opt === correctAnswer) {
+        score++;
+      } else {
+        score = score - 0.25;
+      }
+      scoreEl.textContent = `Score: ${score} / ${totalScore}`;
+
+      nextQuestion();
+    });
   });
-});
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  optionEl.textContent = "";
+  if (currentQuestion >= quesJSON.length) {
+    questionEl.textContent = "Quiz Completed!";
+    nextEl.remove();
+  } else {
+    showQues();
+  }
+}
 
 //Shuffling the options
 function shuffleOptions(options) {
-  for(let i = options.length -1;i>=0;i--){
-    const j = Math.floor(Math.random()*i+1);
+  for (let i = options.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * i + 1);
     [options[i], options[j]] = [options[j], options[i]];
   }
   return options;
 }
-
-
